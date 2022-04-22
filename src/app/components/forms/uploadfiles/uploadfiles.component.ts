eacefe9497f5b5577bec6c99d841ee13855fdded
aemerchan <angel.merchan@zimait.com>
2022-04-21 19:44:16 -0500

    ajuste uploadfiles

--- src/app/components/forms/uploadfiles/uploadfiles.component.ts
+++ src/app/components/forms/uploadfiles/uploadfiles.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
 import { Validators, FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { DataCentralService } from '../../../libs/data-central.service';
 
@@ -13,6 +13,8 @@ export class UploadfilesComponent implements OnInit {
   @Input() metodo: string = '';
 
   @ViewChild('fileInput') fileInput: ElementRef;
+
+  @Output() onUploadSuccess: EventEmitter<any> = new EventEmitter();
 
   public FileForm: FormGroup
   
@@ -38,6 +40,7 @@ export class UploadfilesComponent implements OnInit {
 
     this.dcentral.SubirArchivo(formData, this.metodo).subscribe(resp => {
       this.Limpiar();
+      this.onUploadSuccess.emit();
     })
     
   }
