baf3ba9f36f893c6d2cc056c27a0f04a894e206e
aemerchan <angel.merchan@zimait.com>
2022-04-21 21:17:22 -0500

    Ip ajuste

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'http://52.226.222.167:3001/api'
+  url: 'http://20.102.6.178:3001/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url: 'http://52.226.222.167:3001/api'
+  url: 'http://20.102.6.178:3001/api'
 };
 
 /*
